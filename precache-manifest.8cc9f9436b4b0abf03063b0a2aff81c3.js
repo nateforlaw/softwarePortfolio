0c3fea97c190c99747714eae7c61ee563f321015
nlawrence162 <buddyn32@yahoo.com>
2020-05-11 15:52:47 -0500

    Updates

---
+++ precache-manifest.8cc9f9436b4b0abf03063b0a2aff81c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f221cca75168e37b91a2b2fd9e7dd153",
+    "url": "/softwarePortfolio/index.html"
+  },
+  {
+    "revision": "014d87069d6999a5f0e3",
+    "url": "/softwarePortfolio/static/css/main.3974130c.chunk.css"
+  },
+  {
+    "revision": "d52f2ff11baff8480437",
+    "url": "/softwarePortfolio/static/js/2.ff031452.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/softwarePortfolio/static/js/2.ff031452.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "014d87069d6999a5f0e3",
+    "url": "/softwarePortfolio/static/js/main.72e6c09f.chunk.js"
+  },
+  {
+    "revision": "16fac3577f8674204caf",
+    "url": "/softwarePortfolio/static/js/runtime-main.85ddf58a.js"
+  }
+]);
