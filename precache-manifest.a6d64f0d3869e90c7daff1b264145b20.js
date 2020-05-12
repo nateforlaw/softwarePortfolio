c409c3a85d76f38dd5d6cae4ed75de98fa1a6230
nlawrence162 <buddyn32@yahoo.com>
2020-05-11 22:21:34 -0500

    Updates

--- precache-manifest.a6d64f0d3869e90c7daff1b264145b20.js
+++ precache-manifest.a6d64f0d3869e90c7daff1b264145b20.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e697c17e70495bf31c297f0221b8a3bf",
+    "revision": "6cac9eccc56bab0ce111b1674f27b927",
     "url": "/softwarePortfolio/index.html"
   },
   {
-    "revision": "ad9ad66f569c9e1e8923",
+    "revision": "17f52611127c897a29ad",
     "url": "/softwarePortfolio/static/css/main.c75125e7.chunk.css"
   },
   {
-    "revision": "b79c0320a209706724ae",
-    "url": "/softwarePortfolio/static/js/2.26176a61.chunk.js"
+    "revision": "3974c3c80b7228273623",
+    "url": "/softwarePortfolio/static/js/2.342105e8.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/softwarePortfolio/static/js/2.26176a61.chunk.js.LICENSE.txt"
+    "url": "/softwarePortfolio/static/js/2.342105e8.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "ad9ad66f569c9e1e8923",
-    "url": "/softwarePortfolio/static/js/main.32eb4fc9.chunk.js"
+    "revision": "17f52611127c897a29ad",
+    "url": "/softwarePortfolio/static/js/main.368297d9.chunk.js"
   },
   {
     "revision": "16fac3577f8674204caf",
@@ -26,6 +26,14 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "cb2b5980baad9275fd7d8b5d75711bc0",
     "url": "/softwarePortfolio/static/media/java_desktop.cb2b5980.png"
+  },
+  {
+    "revision": "b23704fcb9fa56bdb956d32c716a34fb",
+    "url": "/softwarePortfolio/static/media/js_uno.b23704fc.png"
+  },
+  {
+    "revision": "db29f9569fb89f3c98e6e7dfd8733722",
+    "url": "/softwarePortfolio/static/media/php_output.db29f956.png"
   },
   {
     "revision": "87014c83aa39bdfa2d9fe42133644e7f",
